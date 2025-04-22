1d881f983310f3f90982eedd31b66dc231d0f9a6
pusteaghayeva <puste.aghayeva@gmail.com>
2025-04-22 17:49:04 +0400

    create new pages

--- src/js/main.js
+++ src/js/main.js
@@ -87,4 +87,14 @@ jQuery(document).ready(function ($) {
 
     });
 
-})(jQuery); 
+})(jQuery);
+
+// activity
+jQuery(document).ready(($) => {
+    $('.as-accordion-head').click(function () {
+        $(this).find('.as-accordion-close').toggleClass('open');
+        $(this).next('.as-accordion-collapse').slideToggle('fast', () => {
+            // Do something maybe .. 
+        });
+    });
+});
